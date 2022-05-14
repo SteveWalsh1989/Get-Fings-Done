de1d1932385496c3953624c7d32187fb052470d0
stevewalsh1989 <stevewalsh89@gmail.com>
2022-05-14 12:19:07 +0100

    add vuetify, compositon API support

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import VueCompositionApi from '@vue/composition-api';
+
+Vue.use(Vuetify);
+Vue.use(VueCompositionApi);
+
+export default new Vuetify({});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>tes{{ test }} </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const test = 'testing yo';
+
+    return { test };
+  },
+};
 </script>
