b2739d40c51b9fa500f11d3691dda704849826fc
stevewalsh1989 <stevewalsh89@gmail.com>
2022-05-21 21:26:41 +0100

    new task and tasklist components

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -1,11 +1,53 @@
-<template></template>
+<template>
+  <v-list-item
+    :class="{ 'blue lighten-5': task.completed }"
+    class="rounded"
+    @click="toggleTaskCompletion(task.id)"
+  >
+    <template v-slot:default>
+      <v-list-item-action>
+        <v-checkbox :input-value="task.completed" color="primary"></v-checkbox>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title :class="{ 'line-through': task.completed }">
+          {{ task.title }}
+        </v-list-item-title>
+      </v-list-item-content>
+
+      <v-list-item-action>
+        <v-btn @click.stop="deleteTask(task.id)" icon>
+          <v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
+        </v-btn>
+    </template>
+  </v-list-item>
+</template>
 
 <script>
+import { computed } from '@vue/composition-api';
+import store from '@/store';
+
 export default {
   name: 'Task',
-  components: { NavBar },
+  components: {},
+  props: {
+    task: { type: Object, required: true },
+  },
   setup() {
-    return {};
+    function toggleTaskCompletion(id) {
+      const tasks = computed(() => store.state.tasks);
+      let task = tasks.value.filter((task) => task.id === id)[0];
+      if (task.completed) {
+        store.commit('uncompleteTask', id);
+      } else {
+        store.commit('completeTask', id);
+      }
+    }
+
+    function deleteTask(id) {
+      store.commit('deleteTask', id);
+    }
+
+    return { deleteTask, toggleTaskCompletion };
   },
 };
 </script>
--- src/components/Tasks/Tasklist.vue
+++ src/components/Tasks/Tasklist.vue
@@ -1,61 +1,21 @@
 <template>
   <v-list subheader two-line flat>
-    <v-list-item
-      v-for="(task, index) in tasks"
-      :key="index"
-      :class="{ 'blue lighten-5': task.completed }"
-      class="rounded"
-      @click="toggleTaskCompletion(task.id)"
-    >
-      <template v-slot:default>
-        <v-list-item-action>
-          <v-checkbox
-            :input-value="task.completed"
-            color="primary"
-          ></v-checkbox>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title :class="{ 'line-through': task.completed }">
-            {{ task.title }}
-          </v-list-item-title>
-        </v-list-item-content>
-
-        <v-list-item-action>
-          <v-btn @click.stop="deleteTask(task.id)" icon>
-            <v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </template>
-    </v-list-item>
+    <Task v-for="(task, index) in tasks" :key="index" :task="task" />
   </v-list>
 </template>
 
 <script>
 import store from '@/store';
-import { computed, ref } from '@vue/composition-api';
-
+import { computed } from '@vue/composition-api';
+import Task from '@/components/Tasks/Task';
 export default {
   name: 'Tasklist',
-  components: {},
+  components: { Task },
   setup() {
     const tasks = computed(() => store.state.tasks);
 
-    function toggleTaskCompletion(id) {
-      let task = tasks.value.filter((task) => task.id === id)[0];
-      if (task.completed) {
-        store.commit('uncompleteTask', id);
-      } else {
-        store.commit('completeTask', id);
-      }
-    }
-
-    function deleteTask(id) {
-      store.commit('deleteTask', id);
-    }
     return {
-      deleteTask,
       tasks,
-      toggleTaskCompletion,
     };
   },
 };
