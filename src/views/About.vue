2e5dcaa3be8a7d4a4f57fd69bd23c9d2fdeb02c1
stevewalsh1989 <stevewalsh89@gmail.com>
2022-05-22 22:00:19 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,35 @@
 <template>
-  <v-container class="">About</v-container>
+  <v-container class="ml-4 mt-4 text-sm grey--text">
+    <h1 class="font-semibold">About this small project</h1>
+    <p class="mt-4">
+      The goal of this mini project is to test out the Vuetify framework by
+      making a small todo task application
+    </p>
+    <v-list dense>
+      <v-subheader>Features</v-subheader>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-for="(item, i) in features" :key="i" disabled>
+          <v-list-item-content>
+            <v-list-item-title v-text="item"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'About',
   setup() {
-    return {};
+    const features = [
+      'Vuetify for components',
+      'Vuex for state management',
+      'Vue Router for routing',
+      'dateFns for date formatting',
+      'vue-demi for vue3 setup format',
+    ];
+    return { features };
   },
 };
 </script>
